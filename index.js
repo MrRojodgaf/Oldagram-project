8bd37273123bfce2194f47120f74fabd15709335
MrRojodgaf <112688440+MrRojodgaf@users.noreply.github.com>
2023-04-03 09:38:31 +0100

    update

--- index.js
+++ index.js
@@ -10,46 +10,46 @@ document.addEventListener('dblclick', function(e){
 
 
 function likeBtn(postId){
-    const likedPost = posts.filter(function(post){
-              return postId == post.id
-              })[0]
-    if (likedPost.isLiked){
-        likedPost.likes--
-    }
-    else {
-        likedPost.likes++ 
-    }
-    likedPost.isLiked = !likedPost.isLiked               
-    renderPage()
+    const likedPost = posts.filter((profile)=>{
+        return postId == profile.id
+            })[0]
+        likedPost.isLiked ? likedPost.likes-- : likedPost.likes++ 
+        likedPost.isLiked = !likedPost.isLiked
+        renderPage()
 }
 
 function renderPage() {
-    let render = ''
-    posts.forEach(function(post){
-      let like = ''
-      if (post.isLiked){
-          like = 'liked'
-      }
-      render +=`<div class="post-header">
-                    <img class="avatar" src=${post.avatar}>
-                    <div>
-                    <h2 class="name">${post.name}</h2>
-                    <h3 class="location">${post.location}</h3>
+    let render = ""
+    posts.map((profile)=>{
+        let like = ''
+        const {isLiked, avatar, name, location, post, id, likes, username, comment} = profile
+        if (isLiked){
+         like = 'liked'
+        }
+        render +=`<div>
+                    <div class="post-header">
+                        <img class="avatar" src=${avatar}>
+                        <div>
+                            <h2 class="name">${name}</h2>
+                            <h3 class="location">${location}</h3>
+                        </div>
+                        <img class="verified" src="https://www.pngitem.com/pimgs/m/302-3024199_instagram-verified-symbol-png-instagram-verified-logo-png.png">
                     </div>
-                    <img class="verified" src="https://www.pngitem.com/pimgs/m/302-3024199_instagram-verified-symbol-png-instagram-verified-logo-png.png">
+                    <img class="post" src='${post}' data-like="${id}">
+                    <div class="react-icons">
+                        <i class="fa-solid fa-heart ${like}" data-like="${id}"></i>
+                        <i class="fa-regular fa-comment"></i>
+                        <i class="fa-regular fa-paper-plane"></i>
+                    </div>
+                    <p class="likes">${likes} likes</p>
+                    <div class="comment">
+                    <p class="username-post"><span>${username}</span>${comment}</p>
+                    </div>
                 </div>
-                <img class="post" src='${post.post}' data-like="${post.id}">
-                <div class="react-icons">
-                    <i class="fa-solid fa-heart ${like}" data-like="${post.id}"></i>
-                    <i class="fa-regular fa-comment"></i>
-                    <i class="fa-regular fa-paper-plane"></i>
-                </div>
-                <p class="likes">${post.likes} likes</p>
-                <p class="username-post"><span>${post.username}</span>${post.comment}</p> 
                 `
     })
-    page.innerHTML = render 
-    }
+    page.innerHTML = render
+}
 
 renderPage()
 
